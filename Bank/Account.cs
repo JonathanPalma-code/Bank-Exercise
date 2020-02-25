ebcd7b702e46d5be9b00d369fdda419b811d3bdc
Jonathan <jonathanpalma89@gmail.com>
2020-02-25 12:38:44 +0000

    add: Account and Print Classes.

---
+++ Bank/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Bank {
+  class Account {
+		private string _header = "Account Details";
+		private int _taxWithdraw = 5;
+		public int AccountId { get; private set; }
+		public string Name { get; set; }
+		public double Amount { get; private set; }
+
+		public Account (int accountId, string name) {
+			AccountId = accountId;
+			Name = name;
+		}
+
+		public string Deposit {
+			set {
+				if (value.Equals("y")) {
+					Console.Write("     Enter a deposit: ");
+					Amount = double.Parse(Console.ReadLine());
+				}
+			}
+		}
+
+		public void DepositAmount(double amount) {
+			_header = "Account details updated";
+			Amount += amount;
+		}
+
+		public void WithdrawAmount(double amount) {
+			_header = "Account details updated";
+			Amount -= amount + _taxWithdraw;
+		}
+
+		public override string ToString() {
+      string h = _header +
+      $"\nAccount Number: {AccountId},";
+      string h1 = h +
+			$" Titular Account: {Name},";
+      string h2 = h1 +
+      $" Montant: £{Amount.ToString("F2")}\n";
+      var result = h2;
+      return result;
+		}
+  }
+}
---
+++ Bank/Print.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Bank {
+	class Print {
+		static void Main(string[] args) {
+			Console.WriteLine("To create an account we will need you to fill those details:");
+			Console.Write("- Account number: ");
+			int accountId = int.Parse(Console.ReadLine());
+			Console.Write("- Full name: ");
+			string name = Console.ReadLine();
+			Console.Write("- Deposit? (y/n): ");
+			string deposit = Console.ReadLine();
+
+			DepositAnswer(deposit);
+
+			Account account = new Account(accountId, name) {
+					Deposit = deposit
+			};
+			Console.WriteLine("\n" + account);
+			Console.Write("Enter an amount to deposit: ");
+			account.DepositAmount(double.Parse(Console.ReadLine()));
+			Console.WriteLine("\n" + account);
+			Console.Write("Enter an amount to withdraw: ");
+			account.WithdrawAmount(double.Parse(Console.ReadLine()));
+			Console.WriteLine("\n" + account);
+		}
+		static void DepositAnswer(string deposit) {
+			while (deposit != "y" && deposit != "n") {
+				Console.WriteLine("Invalid Operation, " +
+					"please insert 'y' for yes and 'n' for no.");
+				deposit = Console.ReadLine();
+			}
+		}
+	}
+}
